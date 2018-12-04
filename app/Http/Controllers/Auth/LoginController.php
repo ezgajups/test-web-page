14a316791926fb0019d1717185456a0323f624a2
Guntis Zarins <guntis.zarinsh@gmail.com>
2018-12-04 15:14:57 +0200

    middlewear alias replaced with class

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Middleware\RedirectIfAuthenticated;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -34,6 +35,6 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->middleware(RedirectIfAuthenticated::class)->except('logout');
     }
 }
