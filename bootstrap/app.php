cb6a8bac0297c5c661beabe223758538a10a4024
Guntis Zarins <guntis.zarinsh@gmail.com>
2018-12-04 13:46:47 +0200

    Experiments with Exception handler overriding

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -38,7 +38,7 @@ $app->singleton(
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
+    Ezgajups\Calculator\Exceptions\CustomDevHandler::class
 );
 
 /*
